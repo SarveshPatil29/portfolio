f0b241e2991731c338604a6b3b04d0126bb84b37
Atharva Bhoite <2020.atharva.bhoite@ves.ac.in>
2022-10-07 11:46:15 +0530

    added Admin Panel

---
+++ pages/adminPanel.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const adminPanel = () => {
+  return (
+    <div>
+      <div className="bg-zinc-900">
+        <span className="self-center flex justify-center text-5xl py-8 font-semibold whitespace-nowrap dark:text-white">
+          ADMIN PANEL
+        </span>
+      </div>
+
+      
+    </div>
+  );
+};
+
+export default adminPanel;
