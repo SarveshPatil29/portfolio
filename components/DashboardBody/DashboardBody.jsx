3cd18b31698b595634ca04229628f6eca4edfeaa
Sarvesh Patil <sarvesh2902@gmail.com>
2022-10-26 20:14:28 +0530

    solved img bug

--- components/DashboardBody/DashboardBody.jsx
+++ components/DashboardBody/DashboardBody.jsx
@@ -1,12 +1,11 @@
 import classes from "./DashboardBody.module.css";
-import Image from "next/image";
 
 export default function DashboardBody() {
     return (
         <section>
             <div className={classes.body}>
-                <Image
-                    src="https://res.cloudinary.com/sarveshp46/image/upload/v1666795090/portfolio/homeimg_o1g1qk.jpg"
+                <img
+                    src="https://res.cloudinary.com/sarveshp46/image/upload/v1666795440/portfolio/homeimg_hri2ki.jpg"
                     alt="home-image"
                 />
             </div>
