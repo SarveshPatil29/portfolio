deb291599a8577b9c05f5aa1bcd53bed9a06347d
Sarvesh Patil <sarvesh2902@gmail.com>
2023-02-07 09:40:22 +0530

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -13,6 +13,15 @@ const Layout = ({ title, content, children }) => {
                     href="https://res.cloudinary.com/drr7rbizq/image/upload/v1664977190/logo-removebg-preview_1_jhx4kw.png"
                 />
             </Head>
+            <!-- Google tag (gtag.js) -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-16ZG18G1H8"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-16ZG18G1H8');
+            </script>
             <div className="">{children}</div>
             
         
